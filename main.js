99528dc5b6338cc67a77e5dd393e60ef352bbdd4
Ion Dodon <iondodon2@gmail.com>
2023-11-13 15:33:38 +0200

    show events

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import * as d3 from 'd3';
 
+const events = [
+    { date: new Date('2021-01-01'), title: 'New Year' },
+    { date: new Date('2021-07-04'), title: 'Independence Day' },
+    // Add more events as needed
+];
+
 // Dimensions
 const width = document.getElementById('timeline').clientWidth;
 const height = document.getElementById('timeline').clientHeight;
@@ -9,12 +15,66 @@ const svg = d3.select('#timeline').append('svg')
     .attr('width', width)
     .attr('height', height);
 
-console.log(new Date())
-
 // Define time scale
 const timeScale = d3.scaleTime()
     .domain([new Date('0001-01-01'), new Date()])
     .range([0, width]);
+
+// Constants for layout
+const lineLength = 30; // Length of the line extending from the dot
+const textOffset = 5;  // How far the text will be from the end of the line
+
+svg.selectAll(".event-line")
+    .data(events)
+    .enter().append("line")
+        .attr("class", "event-line")
+        .attr("x1", d => timeScale(d.date))
+        .attr("y1", height / 2)
+        .attr("x2", d => timeScale(d.date))
+        .attr("y2", height / 2 - lineLength)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1)
+        .style("visibility", "hidden");;
+
+svg.selectAll(".event-text")
+    .data(events)
+    .enter().append("text")
+        .attr("class", "event-text")
+        .attr("x", d => timeScale(d.date))
+        .attr("y", height / 2 - lineLength - textOffset)
+        .attr("text-anchor", "middle") // Center the text over the line
+        .text(d => d.title)
+        .style("visibility", "hidden");
+
+svg.selectAll(".event-dot")
+    .data(events)
+    .enter().append("circle")
+        .attr("class", "event-dot")
+        .attr("cx", d => timeScale(d.date))
+        .attr("cy", height / 2)
+        .attr("r", 5)
+        .attr("fill", "blue")
+    .on("mouseover", function(event, d) {
+        // Show the line and text for the hovered event
+        svg.selectAll(".event-line")
+            .filter(e => e === d)
+            .style("visibility", "visible");
+
+        svg.selectAll(".event-text")
+            .filter(e => e === d)
+            .style("visibility", "visible");
+    })
+    .on("mouseout", function(event, d) {
+        // Hide the line and text when no longer hovering
+        svg.selectAll(".event-line")
+            .filter(e => e === d)
+            .style("visibility", "hidden");
+
+        svg.selectAll(".event-text")
+            .filter(e => e === d)
+            .style("visibility", "hidden");
+    });
+
 
 // Add an axis (for visual reference)
 const axis = svg.append('g')
@@ -31,6 +91,16 @@ function updateAxis(newScale) {
 function zoomed(event) {
     const newScale = event.transform.rescaleX(timeScale);
     updateAxis(newScale);
+
+    svg.selectAll(".event-dot")
+        .attr("cx", d => newScale(d.date));
+
+    svg.selectAll(".event-line")
+        .attr("x1", d => newScale(d.date))
+        .attr("x2", d => newScale(d.date));
+
+    svg.selectAll(".event-text")
+        .attr("x", d => newScale(d.date));
 }
 
 const zoom = d3.zoom()
